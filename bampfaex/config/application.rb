0a3d1027c6ae1103d133d2a1ecff0816959a045d
mcampos-quinn <mcq@berkeley.edu>
2023-11-02 14:14:15 -0700

    upgrade to rails 6.1

--- bampfaex/config/application.rb
+++ bampfaex/config/application.rb
@@ -9,8 +9,8 @@ Bundler.require(*Rails.groups)
 module Bampfaex
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
-    config.secrets = config_for(:secrets)
+    config.load_defaults 6.1
+    config.secrets = Rails.application.config_for(:secrets).with_indifferent_access.dig('options', 'key')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
