92d07fb8f569f351eb171cfe795084728ad83aa5
mcampos-quinn <mcq@berkeley.edu>
2023-11-02 14:51:56 -0700

    upgrade to Rails 7

--- bampfaex/config/application.rb
+++ bampfaex/config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Bampfaex
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.0
     config.secrets = Rails.application.config_for(:secrets).with_indifferent_access.dig('options', 'key')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
